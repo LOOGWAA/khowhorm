dc6a43a904da83dfcd90821908f2d628f301e232
LOOGWAA <110587039+LOOGWAA@users.noreply.github.com>
2022-09-22 12:16:16 +0700

    Add files via upload

--- post.php
+++ post.php
@@ -4,19 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>post</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1 align="center">Webboard KIKI</h1>
+<h1 align="center">Webboard KUKU</h1>
     <hr>
     <div align="center">
-        ต้องการดูกระทู้หมายเลข <?php echo $_GET["id"]; ?><br><br>
-        <table style="width: 10%;  border: 2px solid black;">
-        <tr><td style="background-color: #FF6699;">แสดงความคิดเห็น</td></tr>
-        <tr><td><textarea rows="4" cols="35"></textarea></td></tr>
-        <tr><td align="center"><input type="submit" name="submit" value="ส่งข้อความ"></td></tr>
-        </table><br>
-        <a href="index.html">กลับไปหน้าหลัก</a>
-    </div>
+        ตองการดูกระทู้หมายเลข <?php echo $_GET["id"];?>
+        <br>
+        <?php
+        $n = $_GET["id"];
+        if (($n % 2) == 0)
+        echo "เป็นกระทู้หมายเลขคู่";
+        else
+        echo "เป็นกระทู้หมายเลขคี่";
+        ?>
+        <br>
+        <table style="width: 20%; border: 1px solid black">
+  <tr><td style="background-color: #D6EEEE">แสดงความคิดเห็น</td></tr>
+  <tr><td><textarea></textarea></td></tr>
+  <tr><td colspan="2" align="center"><input type="submit" name="text" value="ส่งข้อความ"></td></tr>
+</table>
+<a href="index.php">กลับไปหน้าหลัก</a>
+</div>
 </body>
 </html>
--- verify.php
+++ verify.php
@@ -4,22 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Verify</title>
+    <title>verify</title>
 </head>
 <body>
-    <h1 align="center">Webboard Kakkak</h1>
+    <h1 align="center">Webboard KUKU</h1>
     <hr>
     <div align="center">
-        เข้าสู่ระบบด้วย<br>
-        login = <?php echo $_POST["login"];?><br>
-        password = <?php echo $_POST["pwd"];?><br>
-      
-      
-      <?php
-         //echo "Login = ".$_POST["login"]."<br>"
-         //echo "Password = ".$_POST["pwd"];
+        <?php
+        if($_POST ["login"] == "admin" && $_POST["pwd"] == "ad1234"){
+            echo "ยินดีต้อนรับ ADMIN";
+        }
+        elseif($_POST ["login"] == "member" && $_POST["pwd"] == "mem1234"){
+            echo "ยินดีต้อนรับ MEMBER";
+        }
+        else
+        echo "ชื่อบัณชีหรือรหัสผ่านไม่ถูกต้อง"
         ?>
-
+        <br>
+        <a href="index.php">กลับไปหน้าหลัก</a>
     </div>
-</body>
+    </body>
 </html>
